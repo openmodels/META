9f7493444e8aeeb085c306ce735f6eeb1137ce52
James Rising <jarising@gmail.com>
2023-07-28 12:02:33 -0400

    also SCC extraction

--- src/scc.jl
+++ src/scc.jl
@@ -47,7 +47,6 @@ function calculate_scc_marginal_national(mm::Union{MarginalModel, MarginalInstan
     #Loop over countries
     results = DataFrame(country=String[], scco2=Float64[])
     for (cc, strcc) in zip((1:dim_count(model, :country)), (dim_keys(model, :country)))
-
         #Calculate consumption per capita for each country
         national_conspc = mm.base[:Consumption, :conspc][pulse_index, cc]
 
@@ -62,40 +61,64 @@ end
 
 ## Monte Carlo SCC calculations
 
-function calculate_scc_base_mc(model::Model, trials::Int64, persist_dist::Bool, emuc_dist::Bool, prtp_dist::Bool, pulse_year::Int64, pulse_size::Float64, emuc::Float64)
+function calculate_scc_base_mc(model::Model, trials::Int64, persist_dist::Bool, emuc_dist::Bool, prtp_dist::Bool, pulse_year::Int64, pulse_size::Float64, emuc::Float64; calc_nationals::Bool=true)
     mm = calculate_scc_setup(model, pulse_year, pulse_size)
 
-    function setsim_base_scc(inst::Union{ModelInstance, MarginalInstance}, draws::DataFrame, ii::Int64, pulse_year::Int64, pulse_size::Float64)
+    function setsim_base_scc(inst::Union{ModelInstance, MarginalInstance}, draws::DataFrame, ii::Int64)
         setsim_base(inst, draws, ii)
         pulse_index = findfirst(dim_keys(model, :time) .== pulse_year)
         inst.modified[:CO2Converter, :co2_extra][pulse_index] = pulse_size
     end
 
+    function getsim_base_scc(inst::Union{ModelInstance, MarginalInstance}, draws::DataFrame; save_rvs::Bool=true)
+        globalscc = calculate_scc_marginal(inst, pulse_year, emuc)
+        if calc_nationals
+            nationalscc = calculate_scc_marginal_national(inst, pulse_year, emuc)
+            push!(nationalscc, ["global", globalscc])
+            nationalscc
+        else
+            globalscc
+        end
+    end
+
     sim_base(mm, trials, persist_dist, emuc_dist, prtp_dist; save_rvs=false,
-             setsim=(inst, draws, ii) -> setsim_base_scc(inst, draws, ii, pulse_year, pulse_size),
-             getsim=(inst, draws; save_rvs) -> calculate_scc_marginal(inst, pulse_year, emuc))
+             setsim=setsim_base_scc,
+             getsim=getsim_base_scc)
 end
 
 if false
     model = base_model(; rcp="RCP4.5", tdamage="pointestimate", slrdamage="mode")
     calculate_scc(model, 2020, 10., 1.5)
+    calculate_scc_national(model, 2020, 10., 1.5)
     sccs = calculate_scc_base_mc(model, 100, false, false, false, 2020, 10., 1.5)
-    [mean(sccs[:other]), std(sccs[:other]), median(sccs[:other])]
+    df = simdataframe(model, sccs, :other, :*)
+    ## [mean(sccs[:other]), std(sccs[:other]), median(sccs[:other])] # This line only works if calc_nationals = false
 end
 
-function calculate_scc_full_mc(model::Model, trials::Int64, pcf_calib::String, amazon_calib::String, gis_calib::String, wais_calib::String, saf_calib::String, ais_dist::Bool, ism_used::Bool, omh_used::Bool, amoc_used::Bool, persist_dist::Bool, emuc_dist::Bool, prtp_dist::Bool, pulse_year::Int64, pulse_size::Float64, emuc::Float64)
+function calculate_scc_full_mc(model::Model, trials::Int64, pcf_calib::String, amazon_calib::String, gis_calib::String, wais_calib::String, saf_calib::String, ais_dist::Bool, ism_used::Bool, omh_used::Bool, amoc_used::Bool, persist_dist::Bool, emuc_dist::Bool, prtp_dist::Bool, pulse_year::Int64, pulse_size::Float64, emuc::Float64; calc_nationals::Bool=true)
     mm = calculate_scc_setup(model, pulse_year, pulse_size)
 
-    function setsim_full_scc(inst::Union{ModelInstance, MarginalInstance}, draws::DataFrame, ii::Int64, ism_used::Bool, omh_used::Bool, amoc_used::Bool, amazon_calib::String, wais_calib::String, ais_dist::Bool, pulse_year::Int64, pulse_size::Float64)
+    function setsim_full_scc(inst::Union{ModelInstance, MarginalInstance}, draws::DataFrame, ii::Int64, ism_used::Bool, omh_used::Bool, amoc_used::Bool, amazon_calib::String, wais_calib::String, ais_dist::Bool)
         setsim_full(inst, draws, ii, ism_used, omh_used, amoc_used, amazon_calib, wais_calib, ais_dist)
         pulse_index = findfirst(dim_keys(model, :time) .== pulse_year)
         inst.modified[:CO2Converter, :co2_extra][pulse_index] = pulse_size
     end
 
+    function getsim_full_scc(inst::Union{ModelInstance, MarginalInstance}, draws::DataFrame; save_rvs::Bool=true)
+        globalscc = calculate_scc_marginal(inst, pulse_year, emuc)
+        if calc_nationals
+            nationalscc = calculate_scc_marginal_national(inst, pulse_year, emuc)
+            push!(nationalscc, ["global", globalscc])
+            nationalscc
+        else
+            globalscc
+        end
+    end
+
     sim_full(mm, trials, pcf_calib, amazon_calib, gis_calib, wais_calib, saf_calib,
              ais_dist, ism_used, omh_used, amoc_used, persist_dist, emuc_dist, prtp_dist; save_rvs=false,
-             setsim=(inst, draws, ii, ism_used, omh_used, amoc_used, amazon_calib, wais_calib, ais_dist) -> setsim_full_scc(inst, draws, ii, ism_used, omh_used, amoc_used, amazon_calib, wais_calib, ais_dist, pulse_year, pulse_size),
-             getsim=(inst, draws; save_rvs) -> calculate_scc_marginal(inst, pulse_year, emuc))
+             setsim=setsim_full_scc,
+             getsim=getsim_full_scc)
 end
 
 if false
@@ -115,7 +138,8 @@ if false
                                  false, # emuc
                                  false, # prtp
                                  2020, 10., 1.5)
-    [mean(sccs[:other]), std(sccs[:other]), median(sccs[:other])]
+    df = simdataframe(model, sccs, :other, :*)
+    ## [mean(sccs[:other]), std(sccs[:other]), median(sccs[:other])] # This line only works if calc_nationals = false
 
     model = full_model(rcp="RCP4.5", ssp="SSP2")
     calculate_scc(model, 2020, 1.0, 1.5)
@@ -133,5 +157,6 @@ if false
                                  false, # emuc
                                  false, # prtp
                                  2020, 10., 1.5)
-    [mean(sccs[:other]), std(sccs[:other]), median(sccs[:other])]
+    df = simdataframe(model, sccs, :other, :*)
+    ## [mean(sccs[:other]), std(sccs[:other]), median(sccs[:other])] # This line only works if calc_nationals = false
 end

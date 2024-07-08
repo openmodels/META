d875667d64e88987277786e3fd42e1f60f4f2ed5
James Rising <jarising@gmail.com>
2024-07-08 11:29:01 -0400

    add docs

--- analysis/gsa.jl
+++ analysis/gsa.jl
@@ -41,18 +41,18 @@ function prep_gsa(do_gas)
 
     yyvec = convert.(Float64, yy[keepobs])
 
-    return (yyvec, XXmat, keepcol)
+    return (yyvec, XXmat, names(XX)[keepcol], keepcol)
 end
 
-function get_gsa(yyvec, XXmat, do_method)
+function get_gsa(yyvec, XXmat, XXnames, do_method)
     if do_method == "easi"
         res = gsa(XXmat, yyvec, EASI())
 
-        DataFrame(param=names(XX)[keepcol], raw=res.S1, lo=res.S1, hi=res.S1)
+        DataFrame(param=XXnames, raw=res.S1, lo=res.S1, hi=res.S1)
     elseif do_method == "delta"
         res = gsa(XXmat, yyvec, DeltaMoment())
 
-        DataFrame(param=names(XX)[keepcol], raw=res.adjusted_deltas, lo=res.adjusted_deltas_low, hi=res.adjusted_deltas_hi)
+        DataFrame(param=XXnames, raw=res.adjusted_deltas, lo=res.adjusted_deltas_low, hi=res.adjusted_deltas_hi)
     else
         res = gsa(XXmat, yyvec', RegressionGSA(do_method == "reg-rank"))
 
@@ -61,13 +61,42 @@ function get_gsa(yyvec, XXmat, do_method)
         else
             partialvar = vec(res.partial_correlation.^2)
         end
-        DataFrame(param=names(XX)[keepcol], raw=partialvar, lo=partialvar, hi=partialvar)
+        DataFrame(param=XXnames, raw=partialvar, lo=partialvar, hi=partialvar)
     end
 end
 
+function get_category_values(inputs, do_method)
+    results = get_gsa(inputs[1], inputs[2], inputs[3], do_method)
+    normed_raw = zeros(length(inputs[4]))
+    normed_raw[inputs[4]] = results.raw ./ sum(results.raw)
+    normed_lo = zeros(length(inputs[4]))
+    normed_lo[inputs[4]] = results.lo ./ sum(results.lo)
+    normed_hi = zeros(length(inputs[4]))
+    normed_hi[inputs[4]] = results.hi ./ sum(results.hi)
+
+    import_raw = Float64[]
+    import_lo = Float64[]
+    import_hi = Float64[]
+    labels = String[]
+
+    for (wedges_range, category) in wedge_to_category
+        indices = collect(wedges_range)
+        width_sum = sqrt(sum(normed_raw[indices].^2))
+        push!(import_raw, width_sum)
+        width_sum = sqrt(sum(normed_lo[indices].^2))
+        push!(import_lo, width_sum)
+        width_sum = sqrt(sum(normed_hi[indices].^2))
+        push!(import_hi, width_sum)
+        push!(labels, category)
+    end
+
+    return import_raw, import_lo, import_hi, labels
+end
+
+titles = Dict{String, String}("easi" => "EASI method", "delta" => "Delta moment method",
+                              "reg-level" => "Regression method (levels)", "reg-rank" => "Regression method (ranks)")
+
 if do_pies
-    titles = Dict{String, String}("easi" => "EASI method", "delta" => "Delta moment method",
-                                  "reg-level" => "Regression method (levels)", "reg-rank" => "Regression method (ranks)")
 
     for do_gas in ["scc", "scch4"]
         plots = []
@@ -76,27 +105,15 @@ if do_pies
         for do_method in ["easi", "delta", "reg-level", "reg-rank"]
             println(do_method)
 
-            results = get_gsa(inputs[1], inputs[2], do_method)
-            normed = zeros(length(inputs[3]))
-            normed[inputs[3]] = results.raw ./ sum(results.raw)
+            import_raw, import_lo, import_hi, labels = get_category_values(inputs, do_method)
 
-            updated_widths = Float64[]
-            labels = String[]
-
-            for (wedges_range, category) in wedge_to_category
-                indices = collect(wedges_range)
-                width_sum = sum(normed[indices])
-                push!(updated_widths, width_sum)
-                push!(labels, category)
-            end
-
-            pie(updated_widths, label=labels, title=titles[do_method], legend=:none, linewidth=0.5, c=palette(:tab10))
+            pie(import_raw, label=labels, title=titles[do_method], legend=:none, linewidth=0.5, c=palette(:tab10))
 
             for (i, label) in enumerate(labels)
-                if updated_widths[i] > 0
+                if import_raw[i] > 0
                     r = rand() / 2 + 0.5
-                    theta0 = sum(updated_widths[1:i-1]) + updated_widths[i] / 2
-                    theta = 2π * theta0 - updated_widths[i] / 2
+                    theta0 = sum(import_raw[1:i-1]) + import_raw[i] / 2
+                    theta = 2π * theta0 - import_raw[i] / 2
                     x = 1.1 * cos(theta)
                     y = 1.1 * sin(theta)
                     annotate!([(r * cos(theta), r * sin(theta), text(label, 10, :black, :center))])
@@ -111,6 +128,38 @@ if do_pies
         Plots.pdf("gsa-$(do_gas).pdf")
     end
 else
+    markers = Dict{String, Tuple{Symbol,Int64,Float64,Symbol}}(
+        "easi" => (:circle, 8, 0.8, :blue),
+        "delta" => (:diamond, 8, 0.8, :green),
+        "reg-level" => (:square, 8, 0.8, :red),
+        "reg-rank" => (:cross, 8, 0.8, :purple))
+
     for do_gas in ["scc", "scch4"]
+        inputs = prep_gsa(do_gas)
+        pp = nothing
+        for do_method in ["easi", "delta", "reg-level", "reg-rank"]
+            println(do_method)
+            import_raw, import_lo, import_hi, labels = get_category_values(inputs, do_method)
+
+            if pp == nothing
+                pp = plot(
+                    import_raw, labels, seriestype=:scatter, label=titles[do_method],
+                    marker=markers[do_method],
+                    xlabel="Variables", ylabel="Relative Importance", legend=:topright)
+            elseif all(import_lo .== import_hi)
+                plot!(pp, import_raw, labels, seriestype=:scatter, label=titles[do_method],
+                      marker=markers[do_method])
+            else
+                for ii in 1:length(labels)
+                    plot!(pp, [import_raw[ii]], [labels[ii]], seriestype=:scatter,
+                          label=ii == 1 ? titles[do_method] : "",
+                          xerror=[(import_raw[ii] - import_lo[ii], import_hi[ii] - import_raw[ii])],
+                          marker=markers[do_method], line=(:solid, 2, :purple))
+                end
+            end
+        end
+
+        display(pp)
+        Plots.pdf("gsa-scatter-$(do_gas).pdf")
     end
 end
